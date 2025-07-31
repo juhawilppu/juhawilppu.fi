5e32700c138320319d24f2abe8316bacba2df7f3
Juha Wilppu <juha.wilppu@gmail.com>
2025-07-31 19:57:17 +0300

    Switch to rsync

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ REMOTE="root@juhawilppu.fi"
 TARGET_DIR="/var/www/html"
 
 echo "Deploying to $REMOTE:$TARGET_DIR"
-scp -r "$SRC_DIR"/* "$REMOTE:$TARGET_DIR"
+rsync -av --delete "$SRC_DIR"/ "$REMOTE:$TARGET_DIR"
 
 echo "✅ Deployed successfully."
